e568c4a0bcb9641fd30d9ef413472a4d04f21122
athlum <athlum5211@outlook.com>
2019-08-29 11:46:15 +0800

    update

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"github.com/labstack/echo"
 	_ "net/http/pprof"
+	"os"
 
 	"github.com/athlum/heroku-testapp-backend/server"
 )
+
+var port string
+
+func init() {
+	port = os.Getenv("PORT")
+}
 
 func main() {
 	e := echo.New()
@@ -13,7 +20,7 @@ func main() {
 	root := e.Group("/")
 	new(server.Server).Register(root)
 
-	if err := e.Start("0.0.0.0:80"); err != nil {
+	if err := e.Start("0.0.0.0:" + port); err != nil {
 		panic(err)
 	}
 }
