1d7ac065981d39a8a2202ab944f732cca979a181
athlum <athlum5211@outlook.com>
2019-08-29 14:37:35 +0800

    simple curd

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"os"
+	"time"
 
 	"github.com/athlum/heroku-testapp-backend/model"
 )
@@ -14,8 +15,17 @@ func (s *Server) Register(g *echo.Group) {
 	sg := g.Group("api")
 
 	sg.GET("/query", s.Query)
+	sg.GET("/insert", s.Insert)
 	sg.GET("/assets", s.QueryAssets)
 	sg.GET("/config", s.Config)
+}
+
+func (s *Server) Insert(c echo.Context) error {
+	u := &model.User{Name: fmt.Sprintf("%v", time.Now())}
+	if err := model.DB().Insert(u); err != nil {
+		return c.String(500, err.Error())
+	}
+	return c.String(200, fmt.Sprintf("insert id: %v", u.ID))
 }
 
 func (s *Server) Query(c echo.Context) error {
@@ -23,7 +33,16 @@ func (s *Server) Query(c echo.Context) error {
 	if err != nil {
 		return c.String(500, err.Error())
 	}
-	return c.String(200, fmt.Sprintf("%v rows", v))
+	r := fmt.Sprintf("%v rows\n", v)
+	us := []*model.User{}
+	_, err = model.DB().Select(&us, "select * from user")
+	if err != nil {
+		return c.String(500, err.Error())
+	}
+	for _, u := range us {
+		r += fmt.Sprintf("Id: %v - Name: %v\n", u.ID, u.Name)
+	}
+	return c.String(200, r)
 }
 
 func (s *Server) QueryAssets(c echo.Context) error {
